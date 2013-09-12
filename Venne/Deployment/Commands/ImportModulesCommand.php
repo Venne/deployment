1161b61a17e7239dfed75a88652f6445b094deac
Josef Kříž <pepakriz@gmail.com>
2013-09-12 16:14:13 +0200

    ImportModulesCommand: Improved symlinks.

--- Venne/Deployment/Commands/ImportModulesCommand.php
+++ Venne/Deployment/Commands/ImportModulesCommand.php
@@ -11,9 +11,20 @@ class ImportModulesCommand extends BaseCommand
 
 		$settings = $this->deploymentManager->getSettings();
 		$sandbox = $this->deploymentManager->getSandbox();
+
+		include_once $sandbox['libsDir'] . '/autoload.php';
+
 		foreach ($settings['modules'] as $module => $items) {
+			$modulePath = strtr($items['path'], array(
+				'%libsDir%' => $sandbox['libsDir'],
+				'%modulesDir%' => $sandbox['modulesDir'],
+			));
+
+			include_once $modulePath . '/Module.php';
+			$instance = new $items['class'];
+
 			$link = $sandbox['resourcesDir'] . "/{$module}Module";
-			$target = "../../vendor/venne/{$module}-module/Resources/public";
+			$target = $modulePath . '/' . $instance->getRelativePublicPath();
 			if (!file_exists($link) && file_exists($target)) {
 				symlink($target, $link);
 			}
